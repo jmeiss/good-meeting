4361747da988d2f24d4ddc6a02e4ed18ee736bc8
Jonathan Meiss <jonathan@meiss.fr>
2012-05-06 01:44:32 +0200

    grr

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.full_host = "http://good-meeting.herokuapp.com/"
+OmniAuth.config.full_host = "http://good-meeting.herokuapp.com"
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], { :scope => 'userinfo.email, userinfo.profile, https://www.googleapis.com/auth/calendar' }
