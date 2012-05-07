0285fe8dd246ad5a20682d38de1622b062c0a240
jmeiss <jonathan@meiss.fr>
2012-05-07 18:34:57 +0200

    try fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def my_account
-    @events ||= current_user.gcal_events
+    client = Google::APIClient.new
+    client.authorization.access_token = self.google_token
+
+    service = client.discovered_api('calendar', 'v3')
+    parameters = {
+      'calendarId'    => 'primary',
+      'singleEvents'  => 'true',
+      'timeMin'       => (Time.zone.now-3.days).iso8601.to_s,
+      'timeMax'       => Time.zone.now.iso8601.to_s
+    }
+
+    result = client.execute api_method: service.events.list, parameters: parameters
+    
+    @events = result.data.items
+    # @events ||= current_user.gcal_events
   end
 
 end
