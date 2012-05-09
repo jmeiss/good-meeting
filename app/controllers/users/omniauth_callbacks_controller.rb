0999b480174ae6156fd3b2d5a2e07a0f6571901c
jmeiss <jonathan@meiss.fr>
2012-05-09 11:22:08 +0200

    minor

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google_oauth2 request.env['omniauth.auth'], current_user
 
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', :kind => 'Google')
       @user.update_attribute :google_token, request.env['omniauth.auth'][:credentials][:token]
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', :kind => 'Google')
+
       sign_in_and_redirect @user, :event => :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth']
+      
       redirect_to root_url
     end
   end
